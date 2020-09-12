73b01315d0dd2e30a344f32f4cac89c6c089b63a
Jake Hamilton <jakehamtexas@gmail.com>
2020-09-12 12:11:22 -0500

    Add ordering test

--- test/mapWithConcurrency.spec.ts
+++ test/mapWithConcurrency.spec.ts
@@ -14,4 +14,19 @@ describe('mapWithConcurrency', () => {
     // assert
     expect(actual).to.deep.eq(expected);
   });
+
+  it('should preserve order of promises', async () => {
+    // arrange
+    const promise1 = Promise.resolve(0);
+    const promise2 = Promise.resolve(1);
+    const expected = [await promise1, await promise2];
+
+    const promiseFns = [() => promise1, () => promise2];
+
+    // act
+    const actual = await mapWithConcurrency(promiseFns);
+
+    // assert
+    expect(actual).to.deep.eq(expected);
+  });
 });
